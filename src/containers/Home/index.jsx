50f47b251fd37ed9a149f32f2a4641000de01daa
Dusky7 <victorsantos12dima@gmail.com>
2023-04-26 19:38:26 -0300

    att Home and Button

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from 'react';
 
+import Button from '../../components/Button';
 import api from '../../services/api';
-import { Background } from './styles';
+import {
+  Background,
+  Container,
+  ContainerButtons,
+  Info,
+  Poster
+} from './styles';
 
 function Home() {
   const [movie, setMovie] = useState();
@@ -23,11 +30,25 @@ function Home() {
         <Background
           img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
         >
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
+          <Container>
+            <Info>
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+
+              <ContainerButtons>
+                <Button red>Assista Agora</Button>
+                <Button>Assista o Trailer</Button>
+              </ContainerButtons>
+            </Info>
+            <Poster>
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt="capa-do-filne"
+              />
+            </Poster>
+          </Container>
         </Background>
       )}
-      ;
     </>
   );
 }
