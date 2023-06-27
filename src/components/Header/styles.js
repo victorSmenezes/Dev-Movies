aef1cdb57f1c95a0886096762d277b36a453cde8
Dusky7 <victorsantos12dima@gmail.com>
2023-06-27 20:31:18 -0300

    Feat. Add styles of header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,12 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   z-index: 4;
+  min-height: 100px;
   position: fixed;
   top: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 12px 50px;
+  background-color: ${(props) =>
+    props.changeBackground ? '#000' : 'transparent'};
+  transition: background-color 0.6s ease-in-out;
 
   img {
     width: 25%;
@@ -24,9 +28,26 @@ export const Li = styled.li`
   font-weight: 600;
   font-size: 28px;
   cursor: pointer;
+  position: relative;
 
   a {
     text-decoration: none;
     color: #ffffff;
+  }
+
+  &::after {
+    content: '';
+    height: 3px;
+    width: ${(props) => (props.isActive ? '100%' : 0)};
+    background-color: #189b20;
+    position: absolute;
+    bottom: -10px;
+    left: 50%;
+    transform: translateX(-50%);
+    transition: width 0.5s ease-in-out;
+  }
+
+  &:hover::after {
+    width: 100%;
   }
 `;
