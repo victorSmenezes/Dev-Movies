53db7cc516a89d3e176a3de333ea89e25722482c
Dusky7 <victorsantos12dima@gmail.com>
2023-09-06 15:51:02 -0300

    Feat. Slide movies added to detais page

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 import { getImages } from '../../utils/getImages';
 import { Container } from './styles';
 
-export default function Card({ item }) {
+export default function Card({ item, movieId }) {
+  const navigate = useNavigate();
+
   return (
-    <Container>
+    <Container onClick={() => navigate(`/detalhe/${movieId}`)}>
       <img src={getImages(item.poster_path || item.profile_path || '')} />
 
       <h3>{item.original_title || item.name || ''}</h3>
