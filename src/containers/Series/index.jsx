9899364693e2c8abd37bc087476c1d12101bd560
VictorS <victorsantos12dima@gmail.com>
2024-01-17 16:24:35 -0300

    Feat: Section series added

--- src/containers/Series/index.jsx
+++ src/containers/Series/index.jsx
@@ -1,9 +1,108 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/Button';
+import Modal from '../../components/Modal';
+import Slider from '../../components/Slider';
+import {
+  getAiringTodaySeries,
+  getPopularSeries,
+  getSeries,
+  getTopSeries
+} from '../../services/getData';
+import { getImages } from '../../utils/getImages';
+import {
+  Background,
+  Container,
+  ContainerButtons,
+  Info,
+  Poster
+} from './styles';
+
 function Series() {
+  const [showModal, setShowModal] = useState(false);
+  const [series, setSeries] = useState();
+  const [airingTodaySeries, setAiringTodaySeries] = useState();
+  const [topSeries, setTopSeries] = useState();
+  const [popularSeries, setPopularSeries] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getAllData() {
+      Promise.all([
+        getSeries(),
+        getPopularSeries(),
+        getAiringTodaySeries(),
+        getTopSeries()
+      ])
+        .then(([series, airingTodaySeries, topSeries, popularSeries]) => {
+          setSeries(series);
+          setAiringTodaySeries(airingTodaySeries);
+          setTopSeries(topSeries);
+          setPopularSeries(popularSeries);
+        })
+        .catch((err) => console.error(err));
+    }
+
+    getAllData();
+  }, []);
+  console.log(series);
   return (
-    <div>
-      <h1>Series</h1>
-      <p>Essa é a Series</p>
-    </div>
+    <>
+      {series && (
+        <Background img={`${getImages(series.backdrop_path)}`}>
+          {showModal && (
+            <Modal serieId={series.id} setShowModal={setShowModal} />
+          )}
+          <Container>
+            <Info>
+              <h1>{series.name}</h1>
+              <p>{series.overview}</p>
+
+              <ContainerButtons>
+                <Button
+                  red
+                  onClick={() => navigate(`/detalhe-serie/${series.id}`)}
+                >
+                  Assista Agora
+                </Button>
+                <Button onClick={() => setShowModal(true)}>
+                  Assista o Trailer
+                </Button>
+              </ContainerButtons>
+            </Info>
+            <Poster>
+              <img
+                src={`https://image.tmdb.org/t/p/original${series.poster_path}`}
+                alt="capa-da-serie"
+              />
+            </Poster>
+          </Container>
+        </Background>
+      )}
+      {airingTodaySeries && (
+        <Slider
+          info={airingTodaySeries}
+          title={'Séries Saindo hoje 🔥'}
+          route={`/detalhe-serie/`}
+        />
+      )}
+      {topSeries && (
+        <Slider
+          info={topSeries}
+          title={'Top Series'}
+          route={`/detalhe-serie/`}
+        />
+      )}
+      {popularSeries && (
+        <Slider
+          info={popularSeries}
+          title={'Series Populares'}
+          route={`/detalhe-serie/`}
+        />
+      )}
+    </>
   );
 }
 
