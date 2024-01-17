cd8512c234379d8854f4a604c82a59144c820438
VictorS <victorsantos12dima@gmail.com>
2024-01-17 16:28:19 -0300

    Fix: Movie/series trailer check

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,32 +1,41 @@
 import { useEffect, useState } from 'react';
 
-import { getMovieVideos } from '../../services/getData';
+import { getMovieVideos, getSerieVideos } from '../../services/getData';
 import { Background, Container } from './styles';
 
-export default function Modal({ movieId, setShowModal }) {
-  const [movie, setMovie] = useState();
+export default function Modal({ movieId, serieId, setShowModal }) {
+  const [videoKey, setVideoKey] = useState();
 
   useEffect(() => {
-    async function getMovies() {
-      setMovie(await getMovieVideos(movieId));
+    async function fetchVideo() {
+      if (movieId) {
+        const movieData = await getMovieVideos(movieId);
+        setVideoKey(movieData);
+      }
+      if (serieId) {
+        const serieData = await getSerieVideos(serieId);
+        setVideoKey(serieData);
+      }
     }
-    getMovies();
-  }, []);
+
+    fetchVideo();
+  }, [movieId, serieId]);
 
   return (
     <Background onClick={() => setShowModal(false)}>
-      {movie && (
+      {videoKey ? (
         <Container>
           <span>&times;</span>
           <iframe
-            src={`https://www.youtube.com/embed/${movie.key}`}
-            title="Youtube Video Player"
+            src={`https://www.youtube.com/embed/${videoKey[0]?.key}`}
+            title="YouTube Video Player"
             height="500px"
             width="100%"
           ></iframe>
         </Container>
+      ) : (
+        ''
       )}
-      ;
     </Background>
   );
 }
